e99bbb687133af1dd65dd57168c9bc6d576679ba
장용범 <dnwkqk@gmail.com>
2020-03-17 22:52:29 +0900

    first commit

---
+++ MEMO/MEMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEMO
+//
+//  Created by 장용범 on 17/03/2020.
+//  Copyright © 2020 장용범. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
