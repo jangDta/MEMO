5e26b303967fc36662ec5ea9027afad2b12ee4fc
장용범 <dnwkqk@gmail.com>
2020-03-20 22:02:45 +0900

    * DateFormatter로 현재날짜 한국어로 포맷하여 가져오기 * 메모 작성해서 CoreData에 추가하는 부분 작성

--- MEMO/MEMO/ViewController.swift
+++ MEMO/MEMO/ViewController.swift
@@ -10,12 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    //Memo가 이미 있으면 편집하게, 없으면 새로 쓰게
+    var alreadyMemo: Memo?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        checkAlreadyMemoAndInit()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    private func checkAlreadyMemoAndInit() {
+        if let memo = alreadyMemo {
+          navigationItem.title = "메모 편집"
+          contentTextView.text = memo.content
+        } else {
+          navigationItem.title = "새 메모"
+          contentTextView.text = ""
+        }
     }
 
     @IBAction func save(_ sender: Any) {
+        
+        let memoContent = contentTextView.text
+        
+        if let alreadyMemo = alreadyMemo {
+          alreadyMemo.content = memoContent
+            CoreDataManager.shared.saveContext { success in
+                if success {
+                    print("편집 완료!")
+                }
+            }
+        } else {
+            CoreDataManager.shared.addMemo(content: memoContent) { success in
+                if success {
+                    print("새 메모 작성 완료!")
+                }
+            }
+        }
+        
         dismiss(animated: true, completion: nil)
     }
     
